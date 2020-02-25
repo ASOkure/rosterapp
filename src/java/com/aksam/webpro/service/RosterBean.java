6f72b7a0e1154579c2639d63818ca1df3db0ea85
ao475 <Akanimo.Okure@uis.cam.ac.uk>
2020-02-25 10:43:26 +0000

    in progress  business logic and queries

--- src/java/com/aksam/webpro/service/RosterBean.java
+++ src/java/com/aksam/webpro/service/RosterBean.java
@@ -6,6 +6,10 @@
 package com.aksam.webpro.service;
 
 import com.aksam.webpro.entity.Player;
+import com.aksam.webpro.entity.Team;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -18,12 +22,13 @@ import javax.persistence.Query;
  *
  * @author ao475
  */
-public class RosterBean {
+public class RosterBean implements Roster, Serializable {
   //CRUD Player
 
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("rosterappPU");
     EntityManager em = emf.createEntityManager();
 
+    //create Player
     public void createPlayer(String id, String name, String position, double salary) {
 
         try {
@@ -37,26 +42,33 @@ public class RosterBean {
 
     }
 
+    // find player
     public Player findPlayer(String Id) {
 
         try {
 
             Player player = em.find(Player.class, Id);
 
-            return player;
+           
 
+        return player;
+        
+        
         } catch (Exception e) {
 
         }
-
-   
+        return null;
     }
-
+   
+    
+// find Player
     public List findPlayerWithName(String name) {
 
         return em.createQuery("SELECT p From Player p  WHERE p.name LIKE :playerName")
                 .setParameter("playerName", name).getResultList();
     }
+    
+    // delete player
 
     public void dropPlayer(String Id) {
 
@@ -68,8 +80,45 @@ public class RosterBean {
 
         }
     }
-}
+    
+    public List<Player> getPlayerTeam(String Id) {
 
+        List<Player> playerList = null;
+
+        try {
+
+            Team team = em.find(Team.class, Id);
+
+            playerList = this.copyPlayerDetails((List<Player>) team.getPlayer());
+
+        }
+        catch( Exception e){
+            
+            
+        }
+        return null;
+
+    }
+
+    private List<Player> copyPlayerDetails(List<Player> players) {
+
+        List<Player> playerdetails = new ArrayList<Player>();
+
+        Iterator<Player> iterator = players.iterator();
+
+        while (iterator.hasNext()) {
+            Player player = (Player) iterator.next();
+
+            Player details = new Player(player.getId(), player.getName(), player.getPosition(), player.getSalary());
+
+            playerdetails.add(details);
+
+        }
+
+        return playerdetails;
+    }
+
+}
          //CRUD League
-    //CRUD Team
+//CRUD Team
 
